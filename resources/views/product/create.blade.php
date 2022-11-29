9f4f670da0c9d78159ddcbad4e942ecaac4b2f56
mwaurallan <allanmwaura2013@gmail.com>
2022-11-29 18:04:48 +0300

    chanes

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -23,7 +23,7 @@
         <div class="row">
         <div class="col-sm-4">
           <div class="form-group">
-            {!! Form::label('name/Barcode', __('product.product_name') . ':*') !!}
+            {!! Form::label('name/Barcodes', __('product.product_names') . ':*') !!}
               {!! Form::text('name', !empty($duplicate_product->name) ? $duplicate_product->name : null, ['class' => 'form-control', 'required',
               'placeholder' => __('product.product_name')]); !!}
           </div>
